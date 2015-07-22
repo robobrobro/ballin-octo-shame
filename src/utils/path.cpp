d83719302905031779687731c2fce227d816ab88
L.J. Hill <foehawk@gmail.com>
2015-07-21 23:58:44 -0400

    Update path code to C++11

--- src/utils/path.cpp
+++ src/utils/path.cpp
@@ -15,8 +15,6 @@
 #include "utils/path.h"
 #include "utils/string.h"
 
-static int is_path_sep(wchar_t c);
-
 std::wstring utils::path::get_cwd(void)
 {
     char * cwd = _get_cwd(NULL, utils::path::MAX_LEN + 1);
@@ -29,60 +27,39 @@ std::wstring utils::path::get_cwd(void)
 
 std::wstring utils::path::get_dir(const std::wstring & path)
 {
-    wprintf(L"path=%ls\n", path.c_str());
     auto pos = path.find_last_of(PATH_SEP);
-    wprintf(L"path=%ls\n", path.c_str());
     if (pos == std::wstring::npos) return path;
-
-    wprintf(L"path=%ls\n", path.substr(0, pos).c_str());
     return path.substr(0, pos);
 }
 
 std::wstring utils::path::join(const wchar_t * path, ...)
 {
     va_list args;
-    wchar_t *s = NULL, *joined_path = 0;
-    int idx = 0;
-    size_t path_len = 0;
+    wchar_t *s = NULL;
 
-    joined_path = (wchar_t *)malloc((utils::path::MAX_LEN + 1) * sizeof(wchar_t));
-    if (!joined_path) return NULL;
-    wcsncpy(joined_path, path, utils::path::MAX_LEN);
-    path_len = wcsnlen(path, utils::path::MAX_LEN);
-    if (is_path_sep(joined_path[path_len - 1]))
-    {
-        joined_path[path_len - 1] = 0;
-        idx = (int)(path_len - 1);
-    }
-    else
-    {
-        joined_path[path_len] = 0;
-        idx = (int)path_len;
-    }
+    // Remove trailing slash, if there is one
+    std::wstring joined_path = path;
+    if (joined_path.compare(joined_path.size() - 1, 1, PATH_SEP) == 0)
+        joined_path = joined_path.substr(0, joined_path.size() - 1);
 
+    // Append each path in the varargs list
     va_start(args, path);
 
-    for (s = va_arg(args, wchar_t *); s && idx < (int)utils::path::MAX_LEN; s = va_arg(args, wchar_t *))
+    for (s = va_arg(args, wchar_t *); s; s = va_arg(args, wchar_t *))
     {
-        path_len = wcsnlen(s, utils::path::MAX_LEN);
+        size_t path_len = wcsnlen(s, utils::path::MAX_LEN);
         if (path_len > 0)
         {
-            if (!is_path_sep(s[0]))
-            {
-                wcsncpy(&joined_path[idx], PATH_SEP, (size_t)(utils::path::MAX_LEN - idx));
-                ++idx;
-            }
+            if (wcsncmp(s, PATH_SEP, 1) != 0)
+                joined_path += PATH_SEP;
 
-            wcsncpy(&joined_path[idx], s, (size_t)(utils::path::MAX_LEN - idx));
-            idx += (int)(wcsnlen(s, utils::path::MAX_LEN)); 
+            joined_path += s;
         }
     }
 
     va_end(args);
     
-    std::wstring p = joined_path;
-    free(joined_path);
-    return p;
+    return joined_path;
 }
 
 bool utils::path::has_ext(const std::wstring & path, const std::wstring & ext)
@@ -95,10 +72,5 @@ bool utils::path::has_ext(const std::wstring & path, const std::wstring & ext)
 std::wstring utils::path::trim_ext(const std::wstring & path)
 {
     return path.substr(0, path.find_last_of(L"."));
-}
-
-static int is_path_sep(wchar_t c)
-{
-    return (c == '/' || c == '\\');
 }
 
